cc9b2bbfe7648037007fc015111a07624646f67e
Muhammed Uluyol <uluyol0@gmail.com>
2017-03-17 17:10:55 -0400

    service/CompactionController: log more compaction-ralated data

--- src/java/org/apache/cassandra/service/CompactionController.java
+++ src/java/org/apache/cassandra/service/CompactionController.java
@@ -21,16 +21,23 @@ package org.apache.cassandra.service;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import org.apache.log4j.Logger;
 
 import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.config.Schema;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.compaction.CompactionManager;
 import org.apache.cassandra.db.compaction.Controllers;
 import org.apache.cassandra.hists.Hists;
 import org.apache.cassandra.hists.NanoClock;
 import org.apache.cassandra.hists.OpLogger;
+import org.apache.cassandra.metrics.CompactionMetrics;
 import org.apache.cassandra.net.MessageIn;
 
 public class CompactionController {
@@ -84,14 +91,26 @@ public class CompactionController {
                         ctlr.record(CompactionManager.instance.getRateLimiter().getRate() / (1024 * 1024), v);
                         input = ctlr.getInput();
                     }
-                    if (input != prevInput) {
-                        CompactionManager.instance.getRateLimiter().setRate(input * 1024 * 1024);
+
+                    // Logging
+                    //
+                    // Log the current rate under any of these conditions:
+                    // - The compaction rate has changed
+                    // - A maximum number of affected requests has been reached
+                    // - A maximum duration since the last log message
+                    Instant now = Instant.now(NanoClock.instance);
+                    if (input != prevInput || prevCount >= 1000 || (prevStart != null && Duration.between(prevStart, now).getSeconds() > 10)) {
                         if (prevStart != null) {
                             OpLogger.compactionRates().recordValue(prevStart,
                                                                    (long) (prevInput * 1024 * 1024),
-                                                                   Long.toString(prevCount));
+                                                                   getAux(prevCount));
                         }
-                        prevStart = Instant.now(NanoClock.instance);
+                        prevCount = 0;
+                        prevStart = now;
+                    }
+                    if (input != prevInput) {
+                        CompactionManager.instance.getRateLimiter().setRate(input * 1024 * 1024);
+                        prevStart = now;
                         prevInput = input;
                         prevCount = 0;
                     }
@@ -101,6 +120,46 @@ public class CompactionController {
                 }
             }
         }).start();
+    }
+
+    private static final Supplier<String> tablesPerLevelSupplier = Suppliers.memoizeWithExpiration(
+        () -> {
+            StringBuilder sb = new StringBuilder();
+            for (String ks : Schema.instance.getKeyspaces()) {
+                boolean printed = false;
+                for (ColumnFamilyStore cfs : Keyspace.open(ks).getColumnFamilyStores()) {
+                    int[] tpl = cfs.getCompactionStrategyManager().getSSTableCountPerLevel();
+                    if (tpl == null || tpl.length == 0) {
+                        continue;
+                    }
+                    printed = true;
+                    sb.append(ks);
+                    sb.append('|');
+                    sb.append(cfs.getTableName());
+                    sb.append('|');
+                    for (int i=0; i < tpl.length; i++) {
+                        if (tpl[i] != 0) {
+                            sb.append(i);
+                            sb.append(':');
+                            sb.append(tpl[i]);
+                            sb.append(';');
+                        }
+                    }
+                    sb.append('!');
+                }
+                if (printed) {
+                    sb.append('~');
+                }
+            }
+            return sb.toString();
+        },
+        50,
+        TimeUnit.MILLISECONDS);
+
+    private static String getAux(long count) {
+        int wipAndPendingCompactions = CompactionManager.instance.getPendingTasks();
+        String tplMap = tablesPerLevelSupplier.get();
+        return "count=" + count + ",levelCount=" + tplMap + ",pending=" + wipAndPendingCompactions;
     }
 
     private void record(MessageIn.MessageMeta meta, Instant end) {
